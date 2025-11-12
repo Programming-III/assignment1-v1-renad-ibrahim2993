bdb25a5623e9258891ecc771a96acef6146b50a4
renad-ibrahim2993 <renad.ibrahim@student.giu-uni.de>
2025-11-12 15:13:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
+    Enclosure(5, 0,"cat", 1,true);
+    Mammal("white", "cow", 4,false);
     
-    
+
+    Bird(3.2,"bird",1,true);
+
+
     return 0;
 }

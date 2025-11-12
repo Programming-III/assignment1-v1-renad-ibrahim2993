514f25acb2935aff7efe0df983516c9f009224bd
renad-ibrahim2993 <renad.ibrahim@student.giu-uni.de>
2025-11-12 14:57:16 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,31 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
 #include "Animal.h"
 
-//define enclosure class here 
+#ifndef UNTITLED2_ENCLOSURE_H
+#define UNTITLED2_ENCLOSURE_H
+#include<iostream>
+#include<string>
+#include "Animal.h"
+using namespace std;
+class Enclosure: public Animal {
+private:
+    Animal* animal;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure();
+    Enclosure(Animal& animal, int capacity, int currentCount, string& name, int age, bool isHungry);
 
-
-
-
-
-
-
-
-#endif
+    void addAnimal(Animal* a);
+    //void displayAnimals();
+    override void displayAnimals() const;
+//getters
+    Animal getAnimal();
+    int getCapacity;
+    int getCurrentCount;
+    //setters
+    void setAnimal(Animal* animal);
+    void setCapacity(int capacity);
+    void setCurrentCount(int currentCount);
+~Enclosure();
+};
+#endif //UNTITLED2_ENCLOSURE_H

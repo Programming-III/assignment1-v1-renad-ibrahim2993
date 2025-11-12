ffdb6b95b1020e5eabbb746ab4ff1befc5770845
renad-ibrahim2993 <renad.ibrahim@student.giu-uni.de>
2025-11-12 14:59:26 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,21 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
-#include <string>
+#ifndef UNTITLED2_VISITOR_H
+#define UNTITLED2_VISITOR_H
+#include<iostream>
+#include<string>
 using namespace std;
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
 
-//define visitor class here
-
-
-
-
-
-
-
-#endif
+public:
+    void displayInfo();
+    //getters
+    string getvisitorName();
+    int ticketsBought();
+    //setters
+    void setVisitorName(string visitorName);
+    void setTicketsBought(int ticketsBought);
+    ~Visitor();
+};
+#endif //UNTITLED2_VISITOR_H

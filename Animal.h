06ca207ab49934a7c7771e13493b5669d1293064
renad-ibrahim2993 <renad.ibrahim@student.giu-uni.de>
2025-11-12 14:55:35 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,27 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#ifndef UNTITLED2_ANIMAL_H
+#define UNTITLED2_ANIMAL_H
+#include<iostream>
 #include <string>
 using namespace std;
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
+    public:
+    Animal();
+    Animal(string& name, int age, bool isHungry);
+    //setters
+    void setName(int name);
+    void setAge(int age);
+    void setIsHungry(bool isHungry);
+    //getters
+    string getName();
+    int getAge();
+    bool getIsHungry();
 
-//define animal class here 
-
-
-
-#endif
+    virtual void display() const;
+    void feed();
+    ~Animal();
+};
+#endif //UNTITLED2_ANIMAL_H

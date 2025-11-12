981db3b674201d8b5973527fd9c977ab23b9dba0
renad-ibrahim2993 <renad.ibrahim@student.giu-uni.de>
2025-11-12 14:56:02 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,30 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+#ifndef UNTITLED2_BIRD_H
+#define UNTITLED2_BIRD_H
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
+class Bird:public Animal {
+private:
+    float wingSpan;
+public:
 
-//define class bird here 
+    Bird();
+    Bird(float wingSpan, string& name, int age, bool isHungry);
+    //setters
+    void setName(int name);
+    void setAge(int age);
+    void setIsHungry(bool isHungry);
+    void set wingSpan(float wingSpan);
 
+    //getters
+    string getName();
+    int getAge();
+    bool getIsHungry();
+    float getWingSpan();
+    override void display() const;
 
+    ~Bird();
+};
 
-
-
-
-
-#endif
+#endif //UNTITLED2_BIRD_H
